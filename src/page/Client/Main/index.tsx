74972c24920dff0638b3405f560f27aa83c8ab53
toanlet <lethitoan1996@gmail.com>
2023-11-01 09:30:53 +0700

    feat: develop change

--- src/page/Client/Main/index.tsx
+++ src/page/Client/Main/index.tsx
@@ -17,53 +17,11 @@ interface PhotoProps {
 }
 
 const Home = () => {
-  const [items, setItems] = useState<PhotoProps[]>([]);
-
-  const getListPhotos = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/photos')
-      .then((response) => response.json())
-      .then((json) => json);
-
-    console.log(res);
-    if (res.length > 0) {
-      setItems(res);
-    }
-  };
-
-  useEffect(() => {
-    getListPhotos();
-  }, []);
-
-  const [counter, setCounter] = useState(0);
-  const handleCount = () => {
-    setCounter(counter + 5);
-    setCounter((n) => n + 1);
-
-    console.log('counter', counter);
-  };
-
-  const [search, setSearch] = useState('');
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
-
-
-
- 
-
   return (
     <div>
-      <h1>Home</h1>
-      {counter}
-
-      <input value={search} onChange={handleChange} />
-      <button onClick={handleCount}>+3</button>
-
+      <p>Develop change</p>
     </div>
   );
 };
 
 export default Home;
-
-
